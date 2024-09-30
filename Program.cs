e241df3c91f51a0a10e6e759d81aeb2ca7d48c6e
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2024-09-30 22:13:29 +0200

    C# Char Initial Commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main (string[] args) 
+        {
+
+            int x =20, y = 40, z = 60;
+
+            //INTEGER
+            int age = -23;
+            Console.WriteLine("Hello World");
+            Console.WriteLine(age);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            long bigNumber = 900000000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+
+            double negative = -55.23D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 4.566F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            age = 10;
+
+            Console.ReadLine();
+        }
+    }
+}
